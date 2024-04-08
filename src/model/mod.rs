dea998f526d14fa014129f31ed775be730f6622c
EdenOttleyHodgson <lh12g22@soton.ac.uk>
2024-04-08 01:57:31 +0100

    Added basic board setup

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,24 +1,82 @@
-use std::sync::mpsc::{Receiver, Sender};
+#![allow(dead_code)]
+
+use std::{
+    collections::HashMap,
+    default,
+    sync::mpsc::{Receiver, Sender},
+    usize,
+};
 
 use log::{error, info};
+use std::collections::hash_map;
 
 use crate::ControlMsg;
 
-struct Game {
+struct Model {
     ui_sender: Sender<ControlMsg>,
     ui_reciever: Receiver<ControlMsg>,
+    game: Game,
 }
-impl Game {
+impl Model {
     fn new(send: Sender<ControlMsg>, recv: Receiver<ControlMsg>) -> Self {
-        Game {
+        Model {
             ui_sender: send,
             ui_reciever: recv,
+            game: Game::new(),
         }
     }
 }
 
+struct Game {
+    board: Board,
+    timer: ChessTimer,
+    which_turn: Side,
+}
+impl Game {
+    fn new() -> Self {
+        let mut board: Board = Board::new();
+        for col in 'a'..='e' {
+            for row in 1..=8 {
+                board.insert(Position(col, row as usize), None);
+            }
+        }
+        Game {
+            board,
+            timer: ChessTimer {},
+            which_turn: Side::White,
+        }
+    }
+}
+
+enum Side {
+    White,
+    Black,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct Position(char, usize);
+
+type Board = HashMap<Position, Option<Piece>>;
+
+#[derive(Default)]
+struct ChessTimer {}
+
+struct Piece {
+    side: Side,
+    piece_type: PieceType,
+}
+
+enum PieceType {
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn,
+}
+
 pub fn init_model(send: Sender<ControlMsg>, recv: Receiver<ControlMsg>) {
-    let game = Game::new(send, recv);
+    let game = Model::new(send, recv);
     if let Err(e) = game.ui_sender.send(ControlMsg::Debug) {
         error!("{}", e)
     };
